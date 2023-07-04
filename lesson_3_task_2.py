74b3a01568814c878247919b8f0f6c33e90b2db8
strong <strong.tretyakov@gmail.com>
2023-07-04 12:56:25 +0300

    Изменена задач_2 урока_3. Добавлен "sep".

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -6,4 +6,4 @@
 print('Введите 5 этапов развития человека:')
 stage_1, stage_2, stage_3, stage_4, stage_5 = input('1 этап:'), input('2 этап:'), input('3 этап:'), \
     input('4 этап:'), input('5 этап:')
-print(f'Этапы: {stage_1} => {stage_2} => {stage_3} => {stage_4} => {stage_5}')
+print(f'Этапы: {stage_1}', stage_2, stage_3, stage_4, stage_5, sep=' => ')
